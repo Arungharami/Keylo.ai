682123c0ae7de3d38f8eb234059d96dcba0c9fde
AG Studio <shovonyou@gmail.com>
2026-03-13 06:38:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8815454603-9a443",
+  "appId": "1:110398375574:web:8c911f8a34204d184064b7",
+  "apiKey": "AIzaSyAvZ5wZXCWqPnCuXtV4Fns3ew8cpxE2upo",
+  "authDomain": "studio-8815454603-9a443.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "110398375574"
+};
